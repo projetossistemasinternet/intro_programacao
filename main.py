b4f8d7187f81db904ee80291b89e9f726e31a35e
andresilvm <142951742+andresilvm@users.noreply.github.com>
2023-11-26 19:55:03 -0300

    first_commit

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
 # ROTAS TAREFAS
 @app.route("/tarefas")
 def tarefas():
